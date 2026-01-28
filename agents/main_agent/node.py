e58e07843f424928cd617692e45c109f0d5e0912
Tharun-10Dragneel <tharundhatri100204@gmail.com>
2026-01-28 12:38:46 +0530

    fix: update import to use backend.utils in agents

--- agents/main_agent/node.py
+++ agents/main_agent/node.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from state import AgentState, WorkflowStep, NodeName
 from llm_config import get_shared_llm
-from utils import generate_pdf_report
+from backend.utils import generate_pdf_report
 from main_agent.tools import extract_rfp_selection, is_scan_request, is_selection_request
 
 
